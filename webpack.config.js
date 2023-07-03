cde71336ced173974bf3524caff613b99a18d79a
Anna Prokopeva <46624560+heyanny2@users.noreply.github.com>
2023-07-03 17:22:58 +0200

    Update webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -13,7 +13,7 @@ export default {
   mode: mode,
   entry: './src/js/index.js',
   output: {
-    path: path.resolve(__dirname, 'img'),
+    path: path.resolve(__dirname, 'dist'),
     clean: true,
   },
   devServer: {
